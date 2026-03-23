41a1964c8fb6f48f4c5588aaf0e33195bd32bc4a
Ruurd Adema <ruurd@owllab.nl>
2026-03-23 21:49:45 +0100

    Remove unused stuff from RDK

--- source/test/CatchMain.cpp
+++
@@ -1,7 +0,0 @@
-//
-// Created by Ruurd Adema on 28/10/2021.
-// Copyright (c) 2021 Sayso. All rights reserved.
-//
-
-#define CATCH_CONFIG_MAIN  // Tell Catch to provide a main() - only do this in one cpp file
-#include <catch2/catch.hpp>
